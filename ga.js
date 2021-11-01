b12a3e9706de55df878cdf35446da902275f6d1e
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2021-11-01 11:18:41 -0400

    beautify files ahead of editing; load experimental extension into chrome

--- ga.js
+++ ga.js
@@ -9,7 +9,9 @@ goog.module('google3.translating.dictionary.chrome.ga');
 
 const Const = goog.require('goog.string.Const');
 const TrustedResourceUrl = goog.require('goog.html.TrustedResourceUrl');
-const {safeScriptEl} = goog.require('safedom');
+const {
+	safeScriptEl
+} = goog.require('safedom');
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-23514435-1']);
@@ -19,14 +21,14 @@ _gaq.push(['_setSampleRate', '10']);
 // background pages, rendering the pageview count meaningless and overwhelming
 // the Analytics service.
 
-(function() {
-  let ga = /** @type {!HTMLScriptElement} */ (document.createElement('script'));
-  ga.type = 'text/javascript';
-  ga.async = true;
-  safeScriptEl.setSrc(
-      ga,
-      TrustedResourceUrl.fromConstant(
-          Const.from('https://ssl.google-analytics.com/ga.js')));
-  var s = document.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(ga, s);
+(function () {
+	let ga = /** @type {!HTMLScriptElement} */ (document.createElement('script'));
+	ga.type = 'text/javascript';
+	ga.async = true;
+	safeScriptEl.setSrc(
+		ga,
+		TrustedResourceUrl.fromConstant(
+			Const.from('https://ssl.google-analytics.com/ga.js')));
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
 })();
