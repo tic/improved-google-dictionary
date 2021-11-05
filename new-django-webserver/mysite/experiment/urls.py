525ff2d2ee2e94ffe9cd727cc9fe9184417f4840
Austin Engle <42854353+amene815@users.noreply.github.com>
2021-11-05 16:32:43 -0400

    Initial commit of the webserver code; A lot needs to be changed before acutally working

---
+++ new-django-webserver/mysite/experiment/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from django.views.generic.base import RedirectView
+
+from . import views
+
+app_name = 'experiment'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('home', views.IndexView.as_view(), name='home'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('consent_form', views.consent_form, name='consent_form'),
+    path('paper_1', views.paper_1, name='paper_1'),
+    path('quiz_1', views.quiz_1, name='quiz_1'),
+    path('paper_2', views.paper_2, name='paper_2'),
+    path('quiz_2', views.quiz_2, name='quiz_2'),
+]
---
+++ new-django-webserver/mysite/experiment/views.py
@@ -0,0 +1,74 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.utils import timezone
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'experiment/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+def consent_form(request):
+    return render(request,'experiment/consent_form.html')
+
+def paper_1(request):
+    return render(request,'experiment/paper_1.html')
+
+def paper_2(request):
+    return render(request,'experiment/paper_2.html')
+
+def quiz_1(request):
+    return render(request,'experiment/quiz_1.html')
+
+def quiz_2(request):
+    return render(request,'experiment/quiz_2.html')
+
+# def consent_form(request):
+#     return render(request,'experiment/consent_form.html')
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'experiment/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'experiment/results.html'
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'experiment/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('experiment:results', args=(question.id,)))
