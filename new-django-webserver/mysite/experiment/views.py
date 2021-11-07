cde78f1df3fbfe2658ea8b85da2c8e62c2a73f4e
Austin Engle <42854353+amene815@users.noreply.github.com>
2021-11-07 14:16:12 -0500

    building the initial paper template

--- new-django-webserver/mysite/experiment/views.py
+++ new-django-webserver/mysite/experiment/views.py
@@ -4,11 +4,48 @@ from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
 
-from .models import Choice, Question
+from .models import Choice, Question, User
+
+def consent_form(request):
+    return render(request,'experiment/consent_form.html')
+
+def paper_s(request):
+    return render(request,'experiment/paper_s.html')
+
+class PaperView(generic.TemplateView):
+    model = User
+    template_name = 'experiment/paper_1.html'
+
+    def get_current_user(self):
+        """
+        Return the last published user
+        """
+        return User.objects.filter(
+            user_t1__lte=timezone.now()
+        ).order_by('-pub_date')[:1]
+
+def paper_2(request):
+    return render(request,'experiment/paper_2.html')
+
+def quiz_s(request):
+    return render(request,'experiment/quiz_s.html')
+
+def quiz_1(request):
+    return render(request,'experiment/quiz_1.html')
+
+def quiz_2(request):
+    return render(request,'experiment/quiz_2.html')
+
+def ux_form(request):
+    return render(request,'experiment/ux.html')
+
+def thanks(request):
+    return render(request,'experiment/thanks.html')
+
 
 
 class IndexView(generic.ListView):
-    template_name = 'experiment/index.html'
+    template_name = 'experiment/old_stuff/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
@@ -20,28 +57,10 @@ class IndexView(generic.ListView):
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')[:5]
 
-def consent_form(request):
-    return render(request,'experiment/consent_form.html')
-
-def paper_1(request):
-    return render(request,'experiment/paper_1.html')
-
-def paper_2(request):
-    return render(request,'experiment/paper_2.html')
-
-def quiz_1(request):
-    return render(request,'experiment/quiz_1.html')
-
-def quiz_2(request):
-    return render(request,'experiment/quiz_2.html')
-
-# def consent_form(request):
-#     return render(request,'experiment/consent_form.html')
-
 
 class DetailView(generic.DetailView):
     model = Question
-    template_name = 'experiment/detail.html'
+    template_name = 'experiment/old_stuff/detail.html'
 
     def get_queryset(self):
         """
@@ -52,7 +71,8 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'experiment/results.html'
+    template_name = 'experiment/old_stuff/results.html'
+
 
 
 def vote(request, question_id):
@@ -72,3 +92,27 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('experiment:results', args=(question.id,)))
+
+
+def set_time(request, t):
+    time = timezone.now()
+    u  = User.objects.filter(user_t1__lte=timezone.now()).order_by('user_t1')[0]
+
+    page_order = ['', 'experiment:paper_1', 'experiment:quiz_1', 'experiment:paper_2', 'experiment:quiz_2', 'experiment:ux']
+    if t == 1:
+        u.user_t1 = time
+
+    elif t == 2:
+        u.user_t2 = time
+
+    elif t == 3:
+        u.user_t3 = time
+
+    elif t == 4:
+        u.user_t4 = time
+
+    elif t == 5:
+        u.user_t5 = time
+
+    u.save()
+    return HttpResponseRedirect(reverse(page_order[t], args=()))
